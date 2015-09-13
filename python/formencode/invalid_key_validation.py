62c3884f763b8b81873767e4cf9baf778923fa4b
masa <masa.guruto@gmail.com>
2015-09-14 01:09:25 +0900

    update invalid key validation

--- python/formencode/invalid_key_validation.py
+++ python/formencode/invalid_key_validation.py
@@ -5,13 +5,15 @@ import formencode
 class MySchema(formencode.schema.Schema):
     name = formencode.validators.String()
     age = formencode.validators.Int()
-    # weight = formencode.validators.Empty()  # Noneは弾けない
-    weight = None
+    weight = formencode.validators.Empty()  # None,''は弾けない
+    # weight = None  # validatorの無効化.allow_extra_fieldsすると通ってしまう
 
 
 # あるキーを含むときinvalid
 if __name__ == '__main__':
     schema = MySchema()
+    schema.ignore_key_missing = True
+    #schema.allow_extra_fields = True
 
     lack_data = {
         'name': 'maki',
@@ -32,7 +34,8 @@ if __name__ == '__main__':
 
     for d in data:
         try:
+            print('------')
             print(schema.to_python(d))
         except formencode.Invalid as e:
             print(e.msg)
-            print(e.value)
+            #print(e.value)
