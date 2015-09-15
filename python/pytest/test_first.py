06567f8a10ab2403822426d6a82544b61c9c9753
vg-1000ch <Masahiko_Izumi@voyagegroup.com>
2015-09-15 17:04:58 +0900

    py.test 操作メモ追加

--- python/pytest/test_first.py
+++ python/pytest/test_first.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# how to test
+#
+# py.test
+# py.test test_first.py
+# py.test -k <exp> [filename]  ignorecaseできないかなー
 
 
 def func(x):
@@ -7,3 +12,15 @@ def func(x):
 
 def test_answer():
     assert func(3) == 5
+
+
+def test_fail():
+    assert False
+
+
+class TestClass:
+    def test_one(self):
+        assert 1 == 1
+
+    def test_two(self):
+        assert 1 == 2
