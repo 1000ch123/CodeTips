27b09a2a024970cd1bcfed55a6bb807351607b4f
vg-1000ch <Masahiko_Izumi@voyagegroup.com>
2015-10-01 10:23:58 +0900

    hookでfetchしてretしたぞ

--- python/sqlalchemy/orm/relation_setter_hook.py
+++ python/sqlalchemy/orm/relation_setter_hook.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Table
+from sqlalchemy import create_engine, Column,\
+    Integer, String, DateTime, Table, event
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship, backref, aliased
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 from sqlalchemy import ForeignKey
 
-from benchmarker import Benchmarker
 
 # マッピング定義のためのベースクラス
 Base = declarative_base()
@@ -16,12 +16,10 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-
-    # こっちに定義してある方が自然かも？
     tags = relationship("Tag", secondary='association', backref='users')
 
     def __repr__(self):
-        return "<User:%s (%s)>" % (self.name, self.tags)
+        return "<User:%s (%s)>" % (self.name, [x.name for x in self.tags])
 
 
 class Tag(Base):
@@ -31,159 +29,49 @@ class Tag(Base):
     name = Column(String(50))
 
     def __repr__(self):
-        return "<Tag:%s>" % self.name
+        return "<Tag:%s (%s)>" % (self.name, [x.name for x in self.users])
 
-# 中間テーブルが余計な情報持たないときはこれでok
-# 中間テーブルがextra columnを持つときはassoc objを持つ必要あり
+
 association_table = Table('association', Base.metadata,
-        Column('user_id', Integer, ForeignKey('users.id')),
-        Column('tag_id', Integer, ForeignKey('tags.id')),
-        Column('created_at', Integer, default=100)
-        )
+                          Column('user_id', Integer, ForeignKey('users.id')),
+                          Column('tag_id', Integer, ForeignKey('tags.id')),
+                          Column('created_at', Integer, default=100)
+                          )
 
-with Benchmarker(width=20, cycle=10) as bench:
 
-    def init_db():
-        # DBへの接続
-        engine = create_engine('sqlite:///:memory:', echo=False)
+@event.listens_for(User.tags, 'append', retval=True)
+def receive_append(target, value, initiator):
+    session = Session()
+    target = session.query(Tag).filter(Tag.id == value).one()
+    return target
 
-        # table schema 作成
-        Base.metadata.create_all(engine)
-        return engine
-
-    def get_session(engine):
-        # セッションクラスの作成
-        Session = sessionmaker()
-        Session.configure(bind=engine)
-        # セッションインスタンスの作成
-        session = Session()
-        return session
-
-    def insert_data(session):
-        # レコードの作成
-        # users
-        maki = User(name='maki')
-        nico = User(name='nico')
-        eli = User(name='eli')
-
-        session.add_all([
-            maki,
-            nico,
-            eli
-            ])
-
-        # tags
-        cool = Tag(name='cool')
-        cute = Tag(name='cute')
-        pure = Tag(name='pure')
-        dere = Tag(name='dere')
-
-        session.add_all([
-            cool,
-            cute,
-            pure,
-            dere
-            ])
-
-        session.commit()
-
-    def op(session):
-        maki = session.query(User).filter(User.name == 'maki').first()
-
-        # simple way
-        # cool = session.query(Tag).filter(Tag.name == 'cool').first()
-        # cute = session.query(Tag).filter(Tag.name == 'cute').first()
-        # dere = session.query(Tag).filter(Tag.name == 'dere').first()
-
-        # relation add
-        # maki.tags.extend([cool, cute, dere])
-
-        # multi add
-        add_tags = session.query(Tag)\
-            .filter(Tag.name.in_(['cool', 'cute', 'pure']))
-        maki.tags.extend(add_tags)
-
-        session.commit()
-        for u in session.query(User).all():
-            print(u)
-
-        for a in session.query(association_table).all():
-            print(a.tag_id)
-            print(a.user_id)
-            print(a.created_at)
-
-        # relation delete
-        # maki.tags.remove(dere)
-        session.commit()
-        for u in session.query(User).all():
-            print(u)
-
-        # relation clear
-        maki.tags.clear()
-        session.commit()
-        for u in session.query(User).all():
-            print(u)
-
-    @bench('id')
-    def op_by_id(bm):
-        engine = init_db()
-        session = get_session(engine)
-        insert_data(session)
-        with bm:
-            # relation add by ids
-            print('--- id insert ---')
-            user_id = 1
-            tag_ids = [1, 2, 3]
-
-            tags = session.query(Tag).filter(Tag.id.in_(tag_ids)).all()
-            user = session.query(User).filter(User.id == user_id).first()
-            user.tags.extend(tags)
-
-            print(session.query(User).filter(User.id == user_id).first())
-            session.rollback()
-
-    @bench('sq')
-    def op_by_id_subquery(bm):
-        engine = init_db()
-        session = get_session(engine)
-        insert_data(session)
-        with bm:
-            # relation add by ids
-            print('--- id insert sub query ---')
-            user_id = 1
-            tag_ids = [1, 2, 3]
-
-            stmt = session.query(Tag).filter(Tag.id.in_(tag_ids))
-            user = session.query(User).filter(User.id == user_id).first()
-            user.tags.extend(stmt)
-
-            print(session.query(User).filter(User.id == user_id).first())
-            session.rollback()
-
-    @bench('direct')
-    def op_by_direct(bm):
-        engine = init_db()
-        session = get_session(engine)
-        insert_data(session)
-        with bm:
-            # relation add by ids
-            print('--- id insert directly ---')
-            user_id = 1
-            tag_ids = [1, 2, 3]
-
-            stmt = session.query(Tag).filter(Tag.id.in_(tag_ids))
-            user = session.query(User).filter(User.id == user_id).first()
-            user.tags.extend(stmt)
-
-            print(session.query(User).filter(User.id == user_id).first())
-            session.rollback()
-
+# DBへの接続
+engine = create_engine('sqlite:///:memory:', echo=False)
+Base.metadata.create_all(engine)
+# セッションクラスの作成
+Session = scoped_session(sessionmaker())
+Session.configure(bind=engine)
 
 if __name__ == '__main__':
-    engine = init_db()
-    session = get_session(engine)
-    insert_data(session)
-    op(session)
-    op_by_id(session)
-    op_by_id_subquery(session)
+    session = Session()
 
+    # master登録
+    nico = User(name='nico')
+    cool = Tag(name='cool')
+    cute = Tag(name='cute')
+
+    session.add(nico)
+    session.add_all([cool, cute])
+    session.commit()
+
+    print('--- master ---')
+    for i in session.query(User).all():
+        print(i)
+
+    # relation 登録
+    nico.tags = [1, 2]
+
+    print('--- adapt relation ---')
+    print(session.query(User).filter(User.name == 'nico').one())
+    for i in session.query(Tag).all():
+        print(i)
