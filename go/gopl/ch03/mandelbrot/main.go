50b5094976a19bf1bb52ca54dd8332eaa2c42e6b
masa <masa.guruto@gmail.com>
2016-01-12 02:19:15 +0900

    ex3-5 red mandelbrot

--- go/gopl/ch03/mandelbrot/main.go
+++ go/gopl/ch03/mandelbrot/main.go
@@ -37,15 +37,20 @@ func main() {
 func mandelbrot(z complex128) color.Color {
 	const iterations = 200
 	const contrast = 15
+	var c color.RGBA
+	c.R = 200
+	c.A = 255
 
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			c.A = 255 - contrast*n
+			c.R = 255 - contrast*n
+			return c
 		}
 	}
-	return color.Black
+	return c
 }
 
 //!-
