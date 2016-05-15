e8d14a4b8feb8f3da61284a0473ebea3c456719c
masa <masa.guruto@gmail.com>
2016-05-16 01:31:58 +0900

    2: add Page Structure

--- go/simple_server/vanilla/main.go
+++ go/simple_server/vanilla/main.go
@@ -10,6 +10,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+
+	//http.HandleFunc("/", handler)
+	//http.ListenAndServe(":8080", nil)
 }
